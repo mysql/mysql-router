c935d04bede556dbc57157c0112ac5c53f14adf8
Mats Kindahl <mats.kindahl@oracle.com>
2015-08-03 20:17:20 +0200

    Fix Doxygen template file location in Docs.cmake.
    Docs.cmake file now read template Doxygen.in relative to the CMakeLists.txt
    that include it.

--- cmake/Docs.cmake
+++ cmake/Docs.cmake
@@ -3,10 +3,10 @@
 find_package(Doxygen)
 
 if (DOXYGEN_FOUND)
-  configure_file(${CMAKE_SOURCE_DIR}/Doxyfile.in ${CMAKE_BINARY_DIR}/Doxyfile)
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
   add_custom_target(docs
-    ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "Generating Doxygen documentation" VERBATIM)
 else()
   message(WARNING "Doxygen not found, no documentation target will be created")
