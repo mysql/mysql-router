8d9faaa2705174e7319c161d0d846b8de22ba9fb
Geert Vanderkelen <geert.vanderkelen@oracle.com>
2015-08-14 13:22:18 +0200

    BUG21641430 Add Debian packaging support
    We add support for creating Debian packages for the following
    Linux distributions:
    
        * Debian 8 (jessie)
        * Ubuntu 15.04 (vivid)
        * Ubuntu 14.04 (trusty)
    
    The Debian packaging files can be found per distribution in the
    packaging folder. The build_deb.sh script was added to make it
    easier to create packages. Provided dependencies (devscripts,
    debhelper and realpath), the script can be used like this:
    
     $ mkdir build && cd build
     $ cmake .. && make package_source
     $ sh ../packaging/debian/build_deb.sh mysql-router-2.0.0.tar.gz
    
    The resulting Debian packages should be available in the current
    working directory.
    
    The installed init script is /etc/init.d/mysqlrouter.
    
    Use chuid in init script; add postinst

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -58,11 +58,6 @@ else()
   message(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER} is not supported")
 endif()
 
-# Always set full RPATH
-set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-
 # Make libraries work while testing/developing
 list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
 if("${isSystemDir}" STREQUAL "-1")
