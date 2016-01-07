ed8a0bb8c399ac3e913e87bf2a5a972ba98f08fe
Amitabh Das <amitab.das@oracle.com>
2016-01-07 15:40:14 +0530

    BUG22195343 Show descriptive error when no plugins are loaded
    We show a descriptive error when no plugins are loaded, saying that Router
    is configured to not load any plugin.

--- src/router/src/router_app.cc
+++ src/router/src/router_app.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -126,6 +126,13 @@ void MySQLRouter::start() {
     }
   }
   loader_->add_logger("INFO");
+
+  std::list<Config::SectionKey> plugins = loader_->available();
+  if (plugins.size() < 2) {
+    std::cout << "MySQL Router not configured to load or start any plugin. Exiting." << std::endl;
+    return;
+  }
+
   try {
     auto log_file = loader_->get_log_file();
     std::cout << "Logging to " << log_file << std::endl;
