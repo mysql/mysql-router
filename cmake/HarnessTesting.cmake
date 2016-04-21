ede510d91a10d50b075d9bc1d2f9d2de83b0680a
Geert Vanderkelen <geert.vanderkelen@oracle.com>
2016-04-21 10:57:53 +0200

    Squashed 'mysql_harness/' changes from 45b9028..130c66a
    130c66a add Path::is_set(); refactoring: resolve some auto declarations
    4503925 Adding support for generating coverage information and report.
    0555e5f Fix GMock variables and ADD_HARNESS_TEST() requires argument
    
    git-subtree-dir: mysql_harness
    git-subtree-split: 130c66a9f1b3e018bab4a5c81c13854f65598fb1

--- cmake/HarnessTesting.cmake
+++ cmake/HarnessTesting.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@ function(ADD_HARNESS_TEST NAME)
     "${_options}" "${_single_value}" "${_multi_value}" ${ARGN})
 
   add_executable(${NAME} ${ADD_HARNESS_TEST_SOURCES})
-  if(requires)
+  if(ADD_HARNESS_TEST_REQUIRES)
     add_dependencies(${NAME} ${ADD_HARNESS_TEST_REQUIRES})
   endif()
   target_link_libraries(${NAME}
