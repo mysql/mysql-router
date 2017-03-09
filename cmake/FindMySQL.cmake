c3c6ef76ceb756e883a146642b332cd5c4d5557a
Jan Kneschke <jan.kneschke@oracle.com>
2017-03-09 15:45:58 +0100

    fix for #25672823: compile fails with openssl
    * removed the dependency of the my_aes_openssl.cc on the mysql headers
      which brings it in line with my_aes_yassl.cc
    * fixed init_keyring_with_keyfile() to only only pass on the actual
      decoded bytes and not the full decode-buffer which fixes
      unit-tests with openssl
    * fixed a type in FindMySQL.cmake

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2015, 2017, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -127,4 +127,4 @@ else()
   endif()
 endif()
 
-mark_as_advanced(MySQL_LIBRARY MySQL_INCLUDE_DIR)
+mark_as_advanced(MySQL_LIBRARY MySQL_INCLUDE_DIRS)
