4a3bdd3b75bd13e20a4bcb114ea7aaee32fcfd54
Andrzej Religa <andrzej.religa@oracle.com>
2017-04-19 16:07:27 +0200

    Added component test for Fix for Bug #25800863 WRONG ERRORMSG IF DIRECTORY IS PROVIDED AS CONFIGFILE

---
+++ tests/component/test_config.cc
@@ -0,0 +1,75 @@
+/*
+  Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; version 2 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "gmock/gmock.h"
+#include "router_component_test.h"
+
+Path g_origin_path;
+
+class RouterConfigTest : public RouterComponentTest, public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    set_origin(g_origin_path);
+    RouterComponentTest::SetUp();
+  }
+};
+
+// Bug #25800863 WRONG ERRORMSG IF DIRECTORY IS PROVIDED AS CONFIGFILE
+TEST_F(RouterConfigTest, RoutingDirAsMainConfigDirectory) {
+  const std::string config_dir = get_tmp_dir();
+
+  // launch the router giving directory instead of config_name
+  auto router = launch_router("-c " +  config_dir);
+
+  EXPECT_TRUE(router.expect_output(
+    "Expected configuration file, got directory name: " + config_dir)
+  ) << "router output: "<< router.get_full_output() << std::endl;
+
+  EXPECT_EQ(router.exit_code(), 1);
+}
+
+// Bug #25800863 WRONG ERRORMSG IF DIRECTORY IS PROVIDED AS CONFIGFILE
+TEST_F(RouterConfigTest, RoutingDirAsExtendedConfigDirectory) {
+  const unsigned ROUTER_PORT = 7015;
+  const unsigned SERVER_PORT = 4417;
+
+  const std::string routing_section =
+                      "[routing:basic]\n"
+                      "bind_port = " + std::to_string(ROUTER_PORT) + "\n"
+                      "mode = read-write\n"
+                      "destinations = 127.0.0.1:" + std::to_string(SERVER_PORT) + "\n";
+
+  std::string conf_file = create_config_file(routing_section);
+  const std::string config_dir = get_tmp_dir();
+
+  // launch the router giving directory instead of an extra config name
+  auto router = launch_router("-c " +  conf_file + " -a " + config_dir);
+
+  EXPECT_TRUE(router.expect_output(
+    "Expected configuration file, got directory name: " + config_dir)
+  ) << "router output: "<< router.get_full_output() << std::endl;
+
+  EXPECT_EQ(router.exit_code(), 1);
+}
+
+
+int main(int argc, char *argv[]) {
+  init_windows_sockets();
+  g_origin_path = Path(argv[0]).dirname();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
