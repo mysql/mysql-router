3d0b5d8c6a3510d87a045463c9024bf35ab413da
Jan Kneschke <jan.kneschke@oracle.com>
2018-03-15 11:19:01 +0100

    removed bogus include(FindProtobuf)
    The check for protobuf is done in CMakeLists.txt already.
    
    On recent cmake versions (3.8, but not 3.5), FindProtobuf() checks if
    the protoc version matches the libprotobuf version which in this case
    causes a warning as the cmake macro when called a 2nd-time isn't aware
    of the bundled protobuf.

--- cmake/mysqlx_protobuf.cmake
+++ cmake/mysqlx_protobuf.cmake
@@ -20,8 +20,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-include(FindProtobuf)
-
 IF(MSVC)
   SET(MYSQLX_PROTOBUF_MSVC_DISABLED_WARNINGS "/wd4267 /wd4244")
 ENDIF()
